7cc3fea30c67a59680c240d9c54374eb3e0c55d4
Jared Loosli <jloosli@gmail.com>
2022-05-05 18:29:50 -0600

    Added AngularFire

--- projects/frontend/src/environments/environment.ts
+++ projects/frontend/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'daily-comms',
+    appId: '1:514735131676:web:a29ce537c75b02c2ad23d9',
+    storageBucket: 'daily-comms.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBKbb4_fw0FHh2_Ip7V7jgY49JIieydBXc',
+    authDomain: 'daily-comms.firebaseapp.com',
+    messagingSenderId: '514735131676',
+  },
   production: false
 };
 

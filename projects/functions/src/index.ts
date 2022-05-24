49bb2daf978f5f6bc9d55fb9afed66e0407fd5c2
Jared Loosli <jloosli@gmail.com>
2022-05-24 16:35:28 -0600

    Webpush functional

--- projects/functions/src/index.ts
+++ projects/functions/src/index.ts
@@ -1,16 +1,47 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-exports.scheduledFunctionCrontab = functions.pubsub.schedule("0 10 * * *").timeZone("America/Denver")
+admin.initializeApp();
+
+exports.scheduledFunctionCrontab = functions.pubsub.schedule('0 10 * * *').timeZone('America/Denver')
   .onRun((context) => {
     // This will be executed every day at 9:00 AM
-    console.log("This should run at 10 am.");
+    console.log('This should run at 10 am.');
     return null;
   });
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.sendPushNotification = functions.https.onRequest(async (request, response) => {
+  const userNumber = request.query.userNumber;
+  const userRef = await admin.firestore().doc(`users/${userNumber}`).get();
+  if (userRef.exists) {
+    const {FCMToken} = userRef.data() as unknown as { FCMToken: string };
+    const payload = {
+      token: FCMToken,
+      webpush:{
+        notification: {
+          title: 'Daily Comm',
+          body: 'Time to click',
+          imageUrl: 'https://app.jrbank.co/assets/images/bank.png',
+        },
+        data: {
+          body: 'Time to click',
+          click_action: 'https://daily-comms.web.app/',
+        },
+        fcm_options: {
+          link: "https://daily-comms.web.app/"
+        }
+      }
+    };
+
+    try {
+      const msgResponse = await admin.messaging().send(payload);
+
+      console.log('Successfully sent message:', msgResponse);
+      response.send('success');
+    } catch (error) {
+      console.log('Error sending message:', error);
+      response.send(`error:<br><pre>${JSON.stringify(error)}</pre>`);
+    }
+  }
+
+});
